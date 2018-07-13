0bcda68c23b533d6905e36a29b48533385de31e3
oxdrapper <carryon8271@gmail.com>
2018-07-13 17:26:19 +0900

    Update id.php

--- id.php
+++ id.php
@@ -5,7 +5,7 @@
 </head>
 <body>
   <a href="index.html">학의재 딱 좋노 이기</a>
-  <form action="id.php">
+  <form action="id2.php">
       <p>비밀번호를 입력해주세요.</p>
       <input type="text" name="password">
       <input type="submit">
